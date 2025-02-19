712e20af827dbb2331843f1fcef22fb4e54da46e
yuki-kunn <21e1248@andrew.ac.jp>
2025-02-19 22:04:03 +0900

    root定義

--- Kansai Sake Night/src/main.tsx
+++ Kansai Sake Night/src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 import Map from './Map.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Map />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
