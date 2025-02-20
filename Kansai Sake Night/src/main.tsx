9fc3e57bd3f16067d766886c4b3fd83da277852b
yuki-kunn <21e1248@andrew.ac.jp>
2025-02-21 00:52:14 +0900

    supabaseつなぎこみ

--- Kansai Sake Night/src/main.tsx
+++ Kansai Sake Night/src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
-import Map from './Map.js'
+import Map from './Map.tsx'
+import Test from './Test.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   </StrictMode>,
