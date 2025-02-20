0d7e45bc5179b9305a00ae51287494e1c55a015b
Asuma09 <241l002@andrew.ac.jp>
2025-02-21 01:09:33 +0900

    fix:表示内容を修正しました。

--- Kansai Sake Night/src/main.tsx
+++ Kansai Sake Night/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import Map from './Map.js'
+import Home from './Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
