e81d6beab16622c0b88d4785519abe091666a40e
yuki-kunn <21e1248@andrew.ac.jp>
2025-02-23 06:37:17 +0900

    css手直し

--- Kansai Sake Night/src/main.tsx
+++ Kansai Sake Night/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
 import Home from './Home.tsx'
 import Map from './Map.tsx'
 import Test from './Test.tsx'
@@ -12,9 +11,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
